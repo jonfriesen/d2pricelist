a61adc63e3ed7bcea5f600b8af707327760a613a
Jon Friesen <jon@jonfriesen.ca>
2021-10-29 17:57:01 -0700

    adds item list

---
+++ src/lib/ItemList.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Item from './Item.svelte';
+</script>
+
+<div class="bg-white shadow overflow-hidden sm:rounded-md">
+	<ul role="list" class="divide-y divide-gray-200">
+		<Item />
+		<Item />
+	</ul>
+</div>
